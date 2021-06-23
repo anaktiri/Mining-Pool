014d0c89f9d32e397d0ad027fb902ae9c10277da
AkhdaN <63330324+anaktiri@users.noreply.github.com>
2021-06-24 00:55:56 +0800

    Update xmrig-monero-hashvault-cpu.sh

--- xmrig-monero-hashvault-cpu.sh
+++ xmrig-monero-hashvault-cpu.sh
@@ -1,5 +1,5 @@
 sudo apt update
 wget https://github.com/anaktiri/xmrig-monero/archive/refs/tags/1.0.0.tar.gz
 tar -zxvf 1.0.0.tar.gz
-cd 1.0.0
+cd xmrig-monero-1.0.0
 ./xmrig -o pool.hashvault.pro:443 -u 83KVe5m37MhFJQvarfAtS6UkJeqK9vtft1eDrkYU4UmUTrqkm1fbzKyeqK1j4dQt2F1rYuGcdL8z5goEHe93QqNN5tpSCxw -p BOT -k --tls
