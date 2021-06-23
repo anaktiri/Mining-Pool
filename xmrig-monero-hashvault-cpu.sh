1ae76247c4acfd04006bfff4e483403d071ca58c
AkhdaN <63330324+anaktiri@users.noreply.github.com>
2021-06-24 01:23:09 +0800

    Update xmrig-monero-hashvault-cpu.sh

--- xmrig-monero-hashvault-cpu.sh
+++ xmrig-monero-hashvault-cpu.sh
@@ -1,5 +1,6 @@
-sudo apt update
-wget https://github.com/anaktiri/xmrig-monero/archive/refs/tags/1.0.0.tar.gz
-tar -zxvf 1.0.0.tar.gz
-cd xmrig-monero-1.0.0
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
 sudo ./xmrig -o pool.hashvault.pro:443 -u 83KVe5m37MhFJQvarfAtS6UkJeqK9vtft1eDrkYU4UmUTrqkm1fbzKyeqK1j4dQt2F1rYuGcdL8z5goEHe93QqNN5tpSCxw -p BOT -k --tls
