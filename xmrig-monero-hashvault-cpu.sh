3c43f4d38efef1c4b1659eddbe0720c8adfca424
AkhdaN <63330324+anaktiri@users.noreply.github.com>
2021-06-24 01:40:11 +0800

    Update xmrig-monero-hashvault-cpu.sh

--- xmrig-monero-hashvault-cpu.sh
+++ xmrig-monero-hashvault-cpu.sh
@@ -1,4 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-bionic-x64.tar.gz
 tar -zxvf xmrig-6.12.2-bionic-x64.tar.gz
 cd xmrig-6.12.2
-./xmrig --donate-level 1 -o monero.herominers.com:10190 -u 83KVe5m37MhFJQvarfAtS6UkJeqK9vtft1eDrkYU4UmUTrqkm1fbzKyeqK1j4dQt2F1rYuGcdL8z5goEHe93QqNN5tpSCxw -p PEKERJA1 -a rx/0 -k
+./xmrig --donate-level 1 --opencl --cuda -o monero.herominers.com:10190 -u 83KVe5m37MhFJQvarfAtS6UkJeqK9vtft1eDrkYU4UmUTrqkm1fbzKyeqK1j4dQt2F1rYuGcdL8z5goEHe93QqNN5tpSCxw -p PEKERJA -a rx/0 -k
